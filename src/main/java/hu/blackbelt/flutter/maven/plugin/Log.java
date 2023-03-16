d5f64b3fd3e4350974033d69afa72f1de440c4ac
gaborflorian <gabor.florian@blackbelt.hu>
2023-03-16 17:43:39 +0100

    Fix tabs and line endsv

--- src/main/java/hu/blackbelt/flutter/maven/plugin/Log.java
+++ src/main/java/hu/blackbelt/flutter/maven/plugin/Log.java
@@ -9,9 +9,9 @@ package hu.blackbelt.flutter.maven.plugin;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,7 +21,7 @@ package hu.blackbelt.flutter.maven.plugin;
  */
 
 public interface Log {
-	
+
     /**
      * Send a message to the user in the <b>debug</b> error level.
      *
--- src/main/java/hu/blackbelt/flutter/maven/plugin/flutter/InstallConfig.java
+++ src/main/java/hu/blackbelt/flutter/maven/plugin/flutter/InstallConfig.java
@@ -9,9 +9,9 @@ package hu.blackbelt.flutter.maven.plugin.flutter;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -39,7 +39,7 @@ final class DefaultInstallConfig implements InstallConfig {
   private final File workingDirectory;
   private final CacheResolver cacheResolver;
   private final Platform platform;
-  
+
   public DefaultInstallConfig(File installDirectory,
                               File tempDirectory,
                               File workingDirectory,
@@ -66,7 +66,7 @@ final class DefaultInstallConfig implements InstallConfig {
   public File getWorkingDirectory() {
     return this.workingDirectory;
   }
-  
+
   public CacheResolver getCacheResolver() {
     return cacheResolver;
   }
